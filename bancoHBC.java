28cb806ec801e57a1ca812a45e7e0166f3b04c8b
Josué Romero <79048209+josuerom@users.noreply.github.com>
2022-11-11 12:18:39 -0500

    Update bancoHBC.java

--- bancoHBC.java
+++ bancoHBC.java
@@ -3,57 +3,49 @@ import java.util.Scanner;
 
 class bancoHBC {
     public static void main(String[] args) {
-        Cuenta obj = new Cuenta(System.in);
-        obj.inicio();
+        int opcion = 0;
+        Scanner sc = new Scanner(System.in);
+        Cuenta obj = new Cuenta(123, "Kevin el loco Moreno", 150000, "CA");
+        
+        /*do {
+            System.out.println("MENU DE OPCIONES. Presione");
+            System.out.println("1. Depositar");
+            System.out.println("2. Retirar");
+            System.out.println("3. Consultar saldo");
+            System.out.println("4. Salir");
+            System.out.println("------------------------------");
+            System.out.print("Digite su opcion: ");
+            opcion = sc.nextInt();
+            
+            switch (opcion) {
+                case 1: System.out.println("Hola");
+                break;
+                case 2: 
+                break;
+                case 3: 
+                break;
+                default: System.out.println("La opcion no existe, gracias por usar nuestro software.");
+            }
+        } while (opcion != 4);*/
     }
 }
 
 class Cuenta {
-    Scanner sc = new Scanner(System.in);
     private int numero_cuenta;
     private String nombre;
     private float saldo;
+    private String tipo_cuenta;
     
-    public Cuenta(int numero_cuenta, String nombre, float saldo) {
+    public Cuenta(int numero_cuenta, String nombre, float saldo, String tipo_cuenta) {
         this.numero_cuenta = numero_cuenta;
         this.nombre = nombre;
         this.saldo = saldo;
-    }
-    
-    public void inicio() {
-        int opción = 0;
-        String cuenta = "";
-        do {
-            System.out.println("MENU DE OPCIONES. Presione");
-            System.out.println("1. Crear cuenta");
-            System.out.println("2. Depositar");
-            System.out.println("3. Retirar");
-            System.out.println("4. Consultar saldo");
-            System.out.println("5. Salir");
-            System.out.println("------------------------------");
-            System.out.print("Digite su opción: ");
-            opcion = sc.nextInt();
-            if (opcion == 1) {
-                System.out.print("Que tipo de cuenta desea [CA o CC]");
-                cuenta = sc.next();
-                cuenta.toUpperCase();
-                if (cuenta.equals("CA")) {
-                    CuentaAhorro obj1 = new CuentaAhorro();
-                    obj1.crearCuenta();
-                } else {
-                    CuentaCorriente obj2 = new CuentaCorriente();
-                    obj2.crearCuenta();
-                }
-            }
-        } while (opcion != 6);
+        this.tipo_cuenta = tipo_cuenta;
     }
 }
 
 class CuentaAhorro extends Cuenta {
     private float interes;
-    
-    public CuentaAhorro() {
-    }
     
     public void crearCuenta() {
     }
@@ -70,10 +62,8 @@ class CuentaAhorro extends Cuenta {
 }
 
 class CuentaCorriente extends Cuenta {
-    private cuatro_por_mil;
+    private float cuatro_por_mil;
     
-    public CuentaCorriente() {
-    }
 
     public void crearCuenta() {
     }
